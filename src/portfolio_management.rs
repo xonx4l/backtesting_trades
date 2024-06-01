c0efc5985ed23bbd8775e48512e7236034076e22
xonx <119700621+xonx4l@users.noreply.github.com>
2024-06-01 21:28:22 +0530

    add structs

--- src/portfolio_management.rs
+++ src/portfolio_management.rs
@@ -1,3 +1,21 @@
-pub fn position_portfolio () {
+use std::collections::HashMap;
+
+#[derive(Debug)] 
+struct Trade {
+    trade_type:String,
+    symbol:String,
+    price:f64,
+    quantity: u32,
+    stop_loss: Option<f64>,
 }
+
+#[derive(Debug)] 
+struct Portfolio {
+    cash:f64,
+    positions: HashMap<String,u32>,
+    stop_losses:HashMap<String,f64>,
+    trade_history: Vec<Trade>,
+    portfolio_value_history: Vec<f64>,
+}
+
 
